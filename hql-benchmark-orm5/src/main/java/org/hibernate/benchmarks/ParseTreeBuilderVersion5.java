39012c2eb89c43aedc35140be06b5a15d830fdff
Andrea Boriero <andrea@hibernate.org>
2018-11-06 13:01:47 +0000

    Removed gradle subprojects in favour of sourceSets

--- hql-benchmark-orm5/src/main/java/org/hibernate/benchmarks/ParseTreeBuilderVersion5.java
+++
@@ -1,9 +0,0 @@
-package org.hibernate.benchmarks;
-
-public class ParseTreeBuilderVersion5 implements HqlParseTreeBuilder {
-
-    @Override
-    public Object getHQLParser(String hqlString) {
-        return 0;
-    }
-}
